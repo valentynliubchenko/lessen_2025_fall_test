00e2c07c954ff4cc2913ea8be894cae849021b7a
Andrii <andrii.onikiienko@nure.ua>
2025-09-06 12:20:27 +0200

    Moya pravka.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "First commit!" << std::endl;
-    std::cout << "First commit!" << std::endl;
+    std::cout << "Giraffe!" << std::endl;
     return 0;
 }
