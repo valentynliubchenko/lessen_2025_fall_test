88b813304a61ef3e7665e5f066bb67b83f3e4033
juliaganzi <ganxijulia006@gmai.com>
2025-09-06 14:50:37 +0300

    my commit

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "First commit!" << std::endl;
-    std::cout << "Giraffe!" << std::endl;
+    std::cout << "my commit" << std::endl;
     return 0;
 }
